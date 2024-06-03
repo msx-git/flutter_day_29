9d80a4175b0159e97bf3802aa642036e888f3f83
Musoxon <msx.tuit@gmail.com>
2024-06-03 10:35:30 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_day_29/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_day_29
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+
+  final List<Widget> pages = [
+    const Center(child: Text('Home')),
+    const Center(child: Text('Search')),
+    const Center(child: Text('Settings')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Navigation Rail"),
+      ),
+      body: Row(
+        children: [
+          NavigationRail(
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Search'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Settings'),
+              ),
+            ],
+            selectedIndex: index,
+            onDestinationSelected: (value) => setState(() => index = value),
+            labelType: NavigationRailLabelType.selected,
+          ),
+          Expanded(child: pages[index]),
+        ],
+      ),
+    );
+  }
+}
